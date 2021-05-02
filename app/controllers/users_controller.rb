a06978a81d454d8e48dd81a07b59bd656504bdec
tomhill991 <tom@mediasia-interactive.com>
2021-05-02 14:11:02 +0800

    fixed null user issue for user_profile_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     before_action :only_see_own_page, only: [:show]
-    
+
     def show
         @user = User.find(params[:id])
     end
-
+    
     def only_see_own_page
         @user = User.find(params[:id])
-      
+        
         if current_user != @user
           redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   root to: 'pages#home'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/privacy', to: 'pages#privacy'
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   get '/users/:id', to: 'users#show', as: 'user_profile'
 
   resources :properties do
